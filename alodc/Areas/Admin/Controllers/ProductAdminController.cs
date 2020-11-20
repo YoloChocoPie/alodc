acb3b65d56f928f2f5bcfc90d0bf7445e0018fdb
Lương Triễn Cường <chocopieyolo@gmail.com>
2020-11-21 00:10:22 +0700

    Update lần 1 - LTC + Tạo Trang quản lý Admin + Tạo  Index Admin + Thay đổi giao diện list thức ăn

---
+++ alodc/Areas/Admin/Controllers/ProductAdminController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using alodc.Models;
+
+namespace alodc.Areas.Admin.Controllers
+{
+    public class ProductAdminController : Controller
+    {
+        QUANLYCANTEENEntities model = new QUANLYCANTEENEntities();
+        // GET: Admin/ProductAdmin
+        public ActionResult Index()
+        {
+            var food = model.FOODs.OrderByDescending(x => x.ID).ToList();
+            return View(food);
+        }
+    }
+}
