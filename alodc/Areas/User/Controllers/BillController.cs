4493b91753305941d9ba0bddd97ea24a281e626b
Lương Triễn Cường <chocopieyolo@gmail.com>
2021-01-08 12:08:58 +0700

    done

---
+++ alodc/Areas/User/Controllers/BillController.cs
@@ -0,0 +1,79 @@
+﻿using alodc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace alodc.Areas.User.Controllers
+{
+    public class BillController : Controller
+    {
+        // GET: User/Bill
+        private AD1TEAM1 db = new AD1TEAM1();
+        private List<ORDER_DETAIL> Cart = null;
+
+        private void GetCartController()
+        {
+           
+            if (Session["Cart"] != null)
+                Cart = Session["Cart"] as List<ORDER_DETAIL>;
+            else
+            {
+                Cart = new List<ORDER_DETAIL>();
+                Session["Cart"] = Cart; 
+            }
+        }
+
+        public ActionResult Index()
+        {
+            var model = db.ORDERs.ToList();
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Create()    
+        {
+            GetCartController();
+            ViewBag.Cart = Cart;
+            ViewBag.account_id = db.ACCOUNTs.OrderByDescending(x => x.ID).ToList();
+            ViewBag.customer_id = db.CUSTOMERs.OrderByDescending(x => x.ID).ToList();
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(ORDER model)
+        {
+            ValidateDonDatHang(model);
+            if (ModelState.IsValid)
+            {
+                db.ORDERs.Add(model);
+                db.SaveChanges();
+                foreach (var item in Cart)
+                {
+                    db.ORDER_DETAIL.Add(new ORDER_DETAIL
+                    {
+                        ORDER_ID = model.ID,
+                            FOOD_ID = item.FOOD_ID,
+                            PRICE = item.FOOD.PRICE,
+                            QUANTITY = item.QUANTITY
+                    }) ;
+                }
+                db.SaveChanges();
+                Session["Cart"] = null;
+                return RedirectToAction("Index", "ProductUser");
+            }
+            GetCartController();
+            ViewBag.Cart = Cart;
+            return View(model);
+        }
+
+        private void ValidateDonDatHang(ORDER model)
+        {
+            GetCartController();
+            if (Cart.Count == 0)
+            {
+                ModelState.AddModelError("", "");
+            }
+        }
+    }
+}
--- alodc/Areas/User/Controllers/CartController.cs
+++ alodc/Areas/User/Controllers/CartController.cs
@@ -1,4 +1,5 @@
-﻿/*using System;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.Entity;
@@ -13,78 +14,42 @@ namespace alodc.Areas.User.Controllers
     public class CartController : Controller
     {
         private AD1TEAM1 db = new AD1TEAM1();
-        private List<MENU> Cart = null;
+        private List<ORDER_DETAIL> Cart = null;
 
-        public CartController()
+        private void GetCartController()
         {
-            var Session = System.Web.HttpContext.Current.Session;
+           
             if (Session["Cart"] != null)
-                Cart = Session["Cart"] as List<MENU>;
+                Cart = Session["Cart"] as List<ORDER_DETAIL>;
             else
             {
-                Cart = new List<MENU>();
-                Session["Cart"] = Cart;
+                Cart = new List<ORDER_DETAIL>();
+                Session["Cart"] = Cart; 
             }
         }
 
         // GET: User/Cart
         public ActionResult Index()
         {
+            GetCartController();
+            var hashtable = new Hashtable();
+            foreach (var order in Cart)
+            {
+                if (hashtable[order.FOOD.ID]!= null)
+                {
+
+                    (hashtable[order.FOOD.ID] as ORDER_DETAIL).QUANTITY += order.QUANTITY;
+                }
+                else hashtable[order.FOOD.ID] = order;
+            }
+            Cart.Clear();
+            foreach (ORDER_DETAIL order in hashtable.Values)
+                Cart.Add(order);
             return View(Cart);
         }
 
-        *//* // GET: User/Cart/Details/5
-         public ActionResult Details(int? id)
-         {
-             if (id == null)
-             {
-                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-             }
-             ORDER_DETAIL oRDER_DETAIL = db.ORDER_DETAIL.Find(id);
-             if (oRDER_DETAIL == null)
-             {
-                 return HttpNotFound();
-             }
-             return View(oRDER_DETAIL);
-         }
- *//*
-        // GET: User/Cart/Create
-        [HttpPost]
-        public ActionResult Create(int foodId, int quantity)
-        {
-            var product = db.FOODs.Find(foodId);
-            Cart.Add(new MENU
-            {
-                FOOD = product,
-                QUANTITY = quantity
-            });
-            return RedirectToAction("Index");
-
-        }
-
-
-
-        // GET: User/Cart/Edit/5
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            MENU oRDER_DETAIL = db.MENUs.Find(id);
-            if (oRDER_DETAIL == null)
-            {
-                return HttpNotFound();
-            }
-            ViewBag.MENU_ID = new SelectList(db.MENUs, "ID", "ID", oRDER_DETAIL.FOOD.MENUs);
-            ViewBag.ORDER_ID = new SelectList(db.ORDERs, "ID", "ORDER_CODE", oRDER_DETAIL.ORDER_DETAIL);
-            return View(oRDER_DETAIL);
-        }
-
-
-
-        // GET: User/Cart/Delete/5
-        public ActionResult Delete(int? id)
+        // GET: User/Cart/Details/5
+        public ActionResult Details(int? id)
         {
             if (id == null)
             {
@@ -98,6 +63,66 @@ namespace alodc.Areas.User.Controllers
             return View(oRDER_DETAIL);
         }
 
+        // GET: User/Cart/Create
+        [HttpPost]
+        public ActionResult Create(int foodId, int quantity)
+        {
+            GetCartController();
+            
+            var food = db.FOODs.Find(foodId);
+            Cart.Add(new ORDER_DETAIL
+            {
+                FOOD = food,
+                QUANTITY = quantity
+               
+            });
+                
+
+
+            return RedirectToAction("Index");
+        }
+
+        
+
+        // GET: User/Cart/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            GetCartController();
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ORDER_DETAIL oRDER_DETAIL = db.ORDER_DETAIL.Find(id);
+            if (oRDER_DETAIL == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.FOOD_ID = new SelectList(db.FOODs, "ID", "FOOD_CODE", oRDER_DETAIL.FOOD_ID);
+            ViewBag.ORDER_ID = new SelectList(db.ORDERs, "ID", "ORDER_CODE", oRDER_DETAIL.ORDER_ID);
+            return View(oRDER_DETAIL);
+        }
+
+        
+
+        // GET: User/Cart/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            GetCartController();
+            var st = Cart.Find(s => s.FOOD_ID == id);
+            Cart.Remove(st);
+           
+            Session["Cart"] = Cart;
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Clear()
+        {
+            GetCartController();
+            Cart.Clear();
+            Session["Cart"] = Cart;
+            return RedirectToAction("Index");
+        }
+
 
         protected override void Dispose(bool disposing)
         {
@@ -109,4 +134,3 @@ namespace alodc.Areas.User.Controllers
         }
     }
 }
-*/
